f06761e9062c047de21fcd752cfccc1381e566fb
nezlobnaya <vlad.burlutskiy@icloud.com>
2020-11-03 15:41:52 -0500

    get node value solution added

---
+++ get_node_value_position_from_tail/get_node_value_position_from_tail.py
@@ -0,0 +1,46 @@
+"""
+Given a pointer to the head of a linked list and a specific position, determine the data value at that position. Count backwards from the tail node. The tail is at postion 0, its parent is at 1 and so on.
+
+"""
+
+
+# Complete the getNode function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def getNode(head, positionFromTail):
+    stack = []
+    while head:
+        stack.append(head.data)
+        head= head.next
+    
+    return stack [-(positionFromTail+ 1)]
+        
+        
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    tests = int(input())
+
+    for tests_itr in range(tests):
+        llist_count = int(input())
+
+        llist = SinglyLinkedList()
+
+        for _ in range(llist_count):
+            llist_item = int(input())
+            llist.insert_node(llist_item)
+
+        position = int(input())
+
+        result = getNode(llist.head, position)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
