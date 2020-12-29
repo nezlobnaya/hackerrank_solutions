31d39126dfc70e1a2e3ddf468bf51a7c4c35033c
nezlobnaya <vlad.burlutskiy@icloud.com>
2020-12-28 21:31:57 -0500

    minor tweaks reversing an array

--- reverse_an_array/reverse_an_array.py
+++ reverse_an_array/reverse_an_array.py
@@ -2,6 +2,11 @@
 
 # Complete the reverseArray function below.
 def reverseArray(a):
-    return a[::-1]
+    # return a[::-1]
+    # return [a[len(a) -1 -i] for i in range(len(a))]
+    aux= []
+    for i in range(len(a)):
+      aux.append(a[len(a) - 1- i])
+    return aux
     
 
