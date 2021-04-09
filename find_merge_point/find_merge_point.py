8d692007dfafb710c2f30d46313907ed521c7235
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2021-04-08 22:34:26 -0400

    find merge point of two ll more optimal solution added

--- find_merge_point/find_merge_point.py
+++ find_merge_point/find_merge_point.py
@@ -7,18 +7,51 @@ Note: After the merge point, both lists will share the same node pointers.
 """
 
 
+# def findMergeNode(head1, head2):
+#     ptr1, ptr2 = head1, head2
+#     while ptr1 and ptr2:
+#         if ptr1 == ptr2:
+#             break
+#         ptr1 = ptr1.next
+#         ptr2 = ptr2.next
+        
+#         if ptr1 == None:
+#             ptr1 = head1
+#         if ptr2 == None:
+#             ptr2 = head2
+#     return ptr1.data
+        
 def findMergeNode(head1, head2):
-    ptr1, ptr2 = head1, head2
-    while ptr1 and ptr2:
-        if ptr1 == ptr2:
-            break
-        ptr1 = ptr1.next
-        ptr2 = ptr2.next
-        
-        if ptr1 == None:
-            ptr1 = head1
-        if ptr2 == None:
-            ptr2 = head2
-    return ptr1.data
-        
+  # O(n)
+  # iterate over the full list
+  # find the diff
+  diff = getListLength(head1) - getListLength(head2)
+  if diff > 0:
+    for i in range(diff):
+      head1 = head1.next
+  elif diff < 0:
+    for i in range(abs(diff)):
+      head2 = head2.next
+  # remove diff # nodes from the longer one (advancing the head)
+  
+  # O(n or m)
+  # iterate over both lists at the same time
+    # if the nodes ==, then return the value
+  while head1 or head2:
+    if head1 == head2:
+      return head1.data
+    head1 = head1.next
+    head2 = head2.next
+      
+  return None
+
+def getListLength(head):
+  # // let pointer = new Node()
+  pointer = head
+  length = 0
+  while pointer:
+    length += 1
+    pointer = pointer.next
+    
+  return length
 
